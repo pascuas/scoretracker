01f49f16e577f97c291104633b38fd6c1c78daeb
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-10-06 16:47:22 -0700

    encrypting/hashing user password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,45 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 //@route POST api/users
 //description - register a user
 //access - Public
-const create = (req, res) => {
+const create = async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("Passed");
+    const { name, email, password } = req.body; 
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ msg: "User already exists" });
+        }
+
+        user = new User({
+            name,
+            email, 
+            password
+        });
+
+        // salt is needed to encrypt the password / genSalt returns a promise so await is needed
+        const salt = await bcrypt.genSalt(10); // this encrypts/hash the password
+
+        // plain text password will be hashed
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send("User saved")
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
 }
 
 
