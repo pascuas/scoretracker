ca818441b9b52611838e041821545a0ba9561900
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-10-22 11:46:11 -0700

    added nav links

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = ({ title }) => {
@@ -8,6 +9,14 @@ const Navbar = ({ title }) => {
             <h1>
                 {title}
             </h1>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
         </div>
     )
 }
