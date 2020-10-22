c591830d81a19a173791926e522f580107262090
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-10-22 11:36:23 -0700

    added some styling for home and about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <Router>
       <Fragment>
         <Navbar />
-        <div className="containter">
+        <div className="container">
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About} />
--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -3,7 +3,13 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            THis is about page
+            <h1>About This App</h1>
+            <p className='my-1'>
+                This a full stack React app for tracking scores from different sports.
+            </p>
+            <p className="bg-dark p">
+                <strong>Version: </strong> 1.0.0
+            </p>
         </div>
     )
 }
