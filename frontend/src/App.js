83811bdbbf80c1177100ae14b672715c0c3d9f6f
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-11-05 10:39:44 -0800

    starting Auth Context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,24 +6,27 @@ import About from './components/pages/About';
 import Favorites from './components/favorites/Favorites';
 
 import TeamState from './context/team/TeamState';
+import AuthState from './context/auth/AuthState';
 import './App.css';
 
 const App = () => {
   return (
-    <TeamState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/favorites' component={Favorites} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </TeamState>
+    <AuthState>
+      <TeamState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/favorites' component={Favorites} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </TeamState>
+    </AuthState>
   );
 }
 
