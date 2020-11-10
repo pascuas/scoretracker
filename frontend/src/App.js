4460b4eaad7b0c6cbe0489089750575917aeb10c
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-11-09 16:56:54 -0800

    added alert context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,26 +9,29 @@ import Login from './components/pages/Login';
 
 import TeamState from './context/team/TeamState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
   return (
     <AuthState>
       <TeamState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/favorites' component={Favorites} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/favorites' component={Favorites} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </TeamState>
     </AuthState>
   );
