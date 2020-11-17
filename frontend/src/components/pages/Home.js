992f7f8c15b487c345936c42acf4ff56f7a52ac8
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-11-17 11:04:30 -0800

    user stays loaded even when the page refreshes

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Results from '../results/Results';
 import {Link} from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
 
 const Home = () => {
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        authContext.loadUser();
+        // eslint-disable-next-line
+    }, []) // only want this to run when component loads so empty bracket is needed
+
     return (
         <div>
             <Link to='/favorites'>Favorites</Link>
