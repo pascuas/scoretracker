a63137d08bc9f9e22dbd43147854e43a26d058ae
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-11-30 13:35:56 -0800

    checkbox added for adding favorites

--- frontend/src/components/favorites/EditFave.js
+++ frontend/src/components/favorites/EditFave.js
@@ -11,12 +11,26 @@ const EditFave = () => {
         // eslint-disable-next-line
     }, []);
 
+    if(teams === null) {
+        return <></>
+    }
+
     console.log('teams', teams)
+    const choices = teams.teams.map((team, index) => {
+        return(
+            <div className="form-check" key={index}>
+                <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
+                <label className="form-check-label" htmlFor="defaultCheck1">{team.strTeam}</label>
+            </div>
+        )
+    })
 
     return (
         <div>
             <h1>This is where I add or delete favorites</h1>
+            {choices}
         </div>
+        
     )
 }
 
