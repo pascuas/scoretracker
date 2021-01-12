86a542e45fec48240b931d58bc3cb558ce4647a5
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2021-01-12 12:52:03 -0800

    private route working

--- frontend/src/components/routing/PrivateRoute.js
+++ frontend/src/components/routing/PrivateRoute.js
@@ -8,7 +8,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     const { isAuthenticated, loading } = authContext;
 
     return (
-        <Route { ...rest } render={props => !isAuthenticated && !loading ? (
+        <Route { ...rest } render={props => !isAuthenticated && loading ? (
             <Redirect to='/' />
         ) : (
             <Component {...props} />
