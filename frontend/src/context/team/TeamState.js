874966a5395e1ffcea60804c54ecf34653d58601
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2021-04-20 13:23:27 -0700

    use of new api

--- frontend/src/context/team/TeamState.js
+++ frontend/src/context/team/TeamState.js
@@ -49,10 +49,17 @@ const TeamState = props => {
     const getNbaResults = async () => {
         try {
             // const res = await axios.get("https://www.thesportsdb.com/api/v1/json/4013017/latestbasketball.php");
-            const res = await axios.get("https://api-nba-v1.p.rapidapi.com/games/date/2021-04-01", {
+            // const res = await axios.get("https://api-nba-v1.p.rapidapi.com/games/date/2021-04-20", {
+            //     headers: {
+            //         'x-rapidapi-key': '7f780f155amsh354c76bc152bd01p18a0eejsn14db727979be',
+            //         'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+            //     }
+            // });
+            const res = await axios.get("https://sportspage-feeds.p.rapidapi.com/games", {
+                params: {league: 'NBA'},
                 headers: {
                     'x-rapidapi-key': '7f780f155amsh354c76bc152bd01p18a0eejsn14db727979be',
-                    'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+                    'x-rapidapi-host': 'sportspage-feeds.p.rapidapi.com'
                 }
             });
             
